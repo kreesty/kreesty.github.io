6d6a5a5f871933a5709dea91a029fb21354fc8da
Kreesty <anihilator.cs@gmail.com>
2023-02-23 16:56:12 +0200

    stuff

---
+++ bidoniera/precache-manifest.f072504b7b2ffa89ccad427863749b72.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9037defcde5cc92f7f3514f34ca05ab4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "55f088edaf03a45aa65f",
+    "url": "./static/css/main.d930d1bd.chunk.css"
+  },
+  {
+    "revision": "d4cb2e4fb2275dc4dff6",
+    "url": "./static/js/2.7f0a4c78.chunk.js"
+  },
+  {
+    "revision": "55f088edaf03a45aa65f",
+    "url": "./static/js/main.84b2f617.chunk.js"
+  },
+  {
+    "revision": "84d9d9be758ffeabd516",
+    "url": "./static/js/runtime-main.fd020d47.js"
+  },
+  {
+    "revision": "931e3476314587a5a996fff0968b7f83",
+    "url": "./static/media/Kebab Website Video.931e3476.mp4"
+  },
+  {
+    "revision": "b856c527e3ac7421fb95adbe39f66412",
+    "url": "./static/media/back.b856c527.svg"
+  },
+  {
+    "revision": "839c50762fd092b2018dadfb75bbfb7b",
+    "url": "./static/media/dontslip-banner.839c5076.png"
+  },
+  {
+    "revision": "03c09063ff580240c875a65165cb91cc",
+    "url": "./static/media/facebook.03c09063.svg"
+  },
+  {
+    "revision": "bde7ed352cf8d7573822c3913adc0a0b",
+    "url": "./static/media/hack-banner-700x459.bde7ed35.png"
+  },
+  {
+    "revision": "5559397268ff1435b155fbdf91e4433b",
+    "url": "./static/media/hack-banner.55593972.png"
+  },
+  {
+    "revision": "3b1987f1cf7b39309e0a9ac6c5575f7d",
+    "url": "./static/media/instagram.3b1987f1.svg"
+  },
+  {
+    "revision": "bf60955cbc568a0f049bac235cf0cb9c",
+    "url": "./static/media/kebab-banner.bf60955c.png"
+  },
+  {
+    "revision": "64cbde6856557104ead27d37cc4134dc",
+    "url": "./static/media/kebab-header.64cbde68.png"
+  },
+  {
+    "revision": "e1c8363677345c20ed23a14e8e534ea8",
+    "url": "./static/media/logo-transparent.e1c83636.png"
+  },
+  {
+    "revision": "341418fe8db76e0ad64f9ba9f11379aa",
+    "url": "./static/media/sitetheme.341418fe.png"
+  },
+  {
+    "revision": "686177246db288136eb1fd95526760f8",
+    "url": "./static/media/sitetheme1_2.68617724.png"
+  },
+  {
+    "revision": "748ec60527e210bfbfc92bb41e7f9a25",
+    "url": "./static/media/sitetheme2.748ec605.png"
+  },
+  {
+    "revision": "8a4707bc783821569ba7351af280117b",
+    "url": "./static/media/spacer-footer.8a4707bc.png"
+  },
+  {
+    "revision": "3984578ec637be696311fb6f8f9240dd",
+    "url": "./static/media/twitter.3984578e.svg"
+  },
+  {
+    "revision": "5e9ffe24b5ff1aa8956ef2c27884b0bf",
+    "url": "./static/media/youtube.5e9ffe24.svg"
+  }
+]);
